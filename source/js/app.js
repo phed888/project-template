540b72dcbcf0b7f4eabcb86bc14a377792d2f934
Brian Speight <brianspeight@Brians-iMac.local>
2015-07-13 21:49:45 -0700

    general adjustments to app.js and style guide

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var browserWidth;
 	var leftColumnPadding = 0;
-	var rightColumnPadding = 0;
+	var rightColumnPadding = 40;
 	var leftColumnReturnButton = 40;
 	var wrapper = $('#wrapper');
 
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		browserWidth = $(window).width();
 		$('.left-column').css('width', browserWidth - leftColumnPadding + leftColumnReturnButton);
 		$('.right-column').css('width', browserWidth - rightColumnPadding - leftColumnReturnButton);
-		wrapper.css('width', browserWidth + browserWidth + leftColumnReturnButton + 10);
+		wrapper.css('width', browserWidth + browserWidth + leftColumnReturnButton + 100);
 	}
 
 	setContentWidth();

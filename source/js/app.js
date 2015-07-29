022c2c526755e2916e468c6b4f0b9b299845b446
Brian Speight <brspeight@expedia.com>
2015-07-28 17:55:32 -0700

    testing some alternatives

--- source/js/app.js
+++ source/js/app.js
@@ -5,31 +5,31 @@ $(document).ready(function() {
 	var leftColumnReturnButton = 40;
 	var wrapper = $('#wrapper');
 
-	function setContentWidth() {
-		browserWidth = $(window).width();
-		$('.left-column').css('width', browserWidth - leftColumnPadding + leftColumnReturnButton);
-		$('.right-column').css('width', browserWidth - rightColumnPadding - leftColumnReturnButton);
-		wrapper.css('width', browserWidth + browserWidth + leftColumnReturnButton + 100);
-	}
+	//function setContentWidth() {
+	//	browserWidth = $(window).width();
+	//	$('.left-column').css('width', browserWidth - leftColumnPadding + leftColumnReturnButton);
+	//	$('.right-column').css('width', browserWidth - rightColumnPadding - leftColumnReturnButton);
+	//	wrapper.css('width', browserWidth + browserWidth + leftColumnReturnButton + 100);
+	//}
 
-	setContentWidth();
+	//setContentWidth();
 
-	$(window).resize(function() {
-		setContentWidth();
-	});
-
-	$('.font-test').click(function() {
-		$(this).next('.holder').slideToggle();
-	});
-	$('.holder').click(function() {
-		if(wrapper.css('margin-left') === '0px') {
-			wrapper.animate({
-				marginLeft: "-=" + browserWidth
-			}, 500);
-		} else {
-			wrapper.animate({
-				marginLeft: 0
-			}, 500);
-		}
-	});
+	//$(window).resize(function() {
+	//	setContentWidth();
+	//});
+	//
+	//$('.font-test').click(function() {
+	//	$(this).next('.holder').slideToggle();
+	//});
+	//$('.holder').click(function() {
+	//	if(wrapper.css('margin-left') === '0px') {
+	//		wrapper.animate({
+	//			marginLeft: "-=" + browserWidth
+	//		}, 500);
+	//	} else {
+	//		wrapper.animate({
+	//			marginLeft: 0
+	//		}, 500);
+	//	}
+	//});
 });
